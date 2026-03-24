768a6faf9f3902cbf572115e4ebb2f6c8333f7b2
Juan Deployit <deployitelias@gmail.com>
2026-03-24 14:52:41 -0300

    fix: adjust React Router for Netlify root domain

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/deployit-e-commerce">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
