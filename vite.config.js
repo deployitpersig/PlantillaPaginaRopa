c031f90fd0094ae4542df810b22e4c6d78839b25
Juan Deployit <deployitelias@gmail.com>
2026-03-24 03:18:03 -0300

    Update dependencies and Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/deployit-e-commerce/',
   plugins: [react()],
   server: {
     open: true
